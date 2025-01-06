d9932f852b95d51285ec6479984e904a4e361a00
alonit <alon.itach@mail.huji.ac.il>
2025-01-06 21:38:10 +0200

    login

--- src/main/java/com/example/frontend/controller/MainController.java
+++ src/main/java/com/example/frontend/controller/MainController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -26,4 +29,21 @@ public class MainController {
     public String radio(Model model) {
         return "radio";
     }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginWithUsernameAndPassword(@RequestParam("email") String email, @RequestParam("password") String password,  RedirectAttributes redirectAttributes) {
+         if ("user@example.com".equals(email) && "password123".equals(password)) {
+             redirectAttributes.addFlashAttribute("toastMessage", email);
+             return "redirect:/";
+         } else {
+             redirectAttributes.addFlashAttribute("error", "Invalid email or password.");
+             return "redirect:/login";
+         }
+    }
+
 }
